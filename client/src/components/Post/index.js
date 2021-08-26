2ea4fd67d67c45cab9438fa0192b1d443bf291ee
EsharkyTheGreat <eshwars1210@gmail.com>
2021-08-26 18:49:00 +0530

    Added markdown-to-jsx

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import post1 from './post1.md'
+import Markdown from 'markdown-to-jsx';
 const Post = (props) => {
+    const [content, setContent] = useState({ md: "" });
+    useEffect(() => {
+        fetch(post1)
+            .then((res) => res.text())
+            .then((md) => {
+                setContent({ md })
+            })
+    }, [])
     return (
         <>
             <h2>Post {parseInt(props.match.params.id, 10)} </h2>
+            <div class="container ht-tm-container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <Markdown children={content.md} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
