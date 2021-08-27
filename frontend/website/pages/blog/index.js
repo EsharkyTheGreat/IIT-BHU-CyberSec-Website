15f204b0988e05d7747af0d8fe2dcc48e599e8cb
EsharkyTheGreat <eshwars1210@gmail.com>
2021-08-27 18:02:26 +0530

    Added and configured REST API

--- frontend/website/pages/blog/index.js
+++ frontend/website/pages/blog/index.js
@@ -1,14 +1,21 @@
 import { Component } from "react"
-
+import getAllBlogPosts from "../../api/getAllBlogPosts.js"
 import Header from "../../components/Header.js"
 import PostCards from "../../components/Postcards.js"
 
 export default class extends Component {
+    static async getInitialProps() {
+        const apiResult = await getAllBlogPosts()
+
+        return {
+            posts: apiResult && apiResult.posts
+        }
+    }
     render() {
         return (
             <>
                 <Header />
-                <PostCards />
+                <PostCards {...this.props} />
             </>
         )
     }
